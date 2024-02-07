bb94af5ddd99a415bf6e955e6c5ebd2ec425f6a7
Atendimento2005 <gogo.june2005@gmail.com>
2024-02-07 19:24:03 +0530

    final commit

--- analyse-score.py
+++ analyse-score.py
@@ -23,10 +23,10 @@ for qid in responses:
 print("Correct:", len(correct))
 print("Incorrect:", len(incorrect))
 print("Unattempted:", len(unattempted)-15)
-print(f"Marks: {len(correct)*4}/-{len(incorrect)} = {len(correct)*4 - len(incorrect)}")
+print(f"\nMarks: {len(correct)*4}/-{len(incorrect)} = {len(correct)*4 - len(incorrect)}")
 
 def printMistakes():
-	print("Mistakes:")
+	print("\nMistakes:")
 	for qid in incorrect:
 		print(f"Question ID: {qid} \nYour Answer: {responses[qid]['option']} \nGiven Answer: {answers[qid]['answer']} \n")
 
